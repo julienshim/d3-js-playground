f5ed2a4fc8f66d10ecac7a4a93b4c9605e496ebd
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-04-30 22:39:07 -0700

    setResult typos

--- webpack-playground/src/app/app.js
+++ webpack-playground/src/app/app.js
@@ -1,19 +1,18 @@
-import { inputsAreValid } from './utils/inputs-are-valid';
+import { inputsAreValid } from "./utils/inputs-are-valid";
 import { parseInputs } from "./utils/parse-inputs";
-
 export const run = (alertService, componentService) => {
   alertService.hideErrors();
+
   componentService.onClick(() => {
     alertService.hideErrors();
     const inputs = componentService.getInputs();
     const parsedInputs = parseInputs(...inputs);
-    if(inputsAreValid(...parsedInputs)) {
+    if (inputsAreValid(...parsedInputs)) {
       const [numA, numB] = parsedInputs;
-      componentService.setResults(numA + numB);
+      componentService.setResult(numA + numB);
     } else {
-      componentService.setResults("");
+      componentService.setResult("");
       alertService.handleAdditionError(inputs, parsedInputs);
     }
   });
 };
-
