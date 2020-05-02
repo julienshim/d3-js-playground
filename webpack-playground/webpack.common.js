5bf878cec1eda35467439054a3b59c32e025fa26
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-05-01 23:19:18 -0700

    webpack split to dev common (rename from config) and prod, plus scss start build test

--- webpack-playground/webpack.common.js
+++ webpack-playground/webpack.common.js
@@ -2,15 +2,12 @@ const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
-  output: {
-    filename: "main.[contentHash].js",
-    path: path.resolve(__dirname, "dist")
-  },
-  plugins: [new HtmlWebpackPlugin({
-    template: "./src/template.html"
-  })],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html"
+    })
+  ],
   module: {
     rules: [
       {
